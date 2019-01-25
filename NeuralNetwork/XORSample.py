9d022644b7c12e4a8ce829375cf49aa31c123e14
yutingchen <wolfchenxing@126.com>
2019-01-25 08:01:28 +0800

    简单非线性关系数据集测试（XOR）

--- NeuralNetwork/XORSample.py
+++ NeuralNetwork/XORSample.py
@@ -1,6 +1,16 @@
+"""
+简单非线性关系数据集测试（XOR）
+ X              Y
+0 0             0
+0 1             1
+1 0             1
+1 1             0
+"""
 from NeuralNetwork import NeuralNetwork
 import numpy as np
 
+
+print(__doc__)
 
 XORArray = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
@@ -10,5 +20,3 @@ y = np.array([0, 1, 1, 0])
 nn.fit(X, y)
 for i in range(len(XORArray)):
     print("样本：%s，属于：%s，预测：%s" % (XORArray[i], y[i], nn.predict(XORArray[i])))
-
-
